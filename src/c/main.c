e9408824c70ba5be40ac62c6c7b99b2d3f79fa48
dmill118 <dmiller182@gmail.com>
2016-10-22 18:02:43 -0400

    Splash screen changes
    Updated splash screen text, added barebones login checking

--- src/c/main.c
+++ src/c/main.c
@@ -2,16 +2,27 @@
 #include "splashWindow.h"
 #include "mainWindow.h"
 
+int logged_in = false;
+
 void launch_main_window(void *data){
   main_window_create();
   window_stack_push(main_window_get_window(), true);
+}
+
+void check_login(){
+  //query android for login status
+  if (logged_in) 
+  {
+    launch_main_window(NULL);
+  }
 }
 
 int main(){
   splash_window_create();
   window_stack_push(splash_window_get_window(), true);
 
-  app_timer_register(1500, launch_main_window, NULL);
+  //app_timer_register(1500, launch_main_window, NULL);
+  check_login();
 
   app_event_loop();
   splash_window_destroy();
--- src/c/splashWindow.c
+++ src/c/splashWindow.c
@@ -2,22 +2,37 @@
 #include "splashWindow.h"
 
 Window *splashWindow;
-TextLayer *splashImageTitle;
+TextLayer *splashImageTitle, *splashMessage;
 
 void splash_window_load(Window *window){
   Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
   
-  splashImageTitle = text_layer_create(GRect(0, 0, 144, 168));
-  text_layer_set_text(splashImageTitle, "Splash text!");
+  splashImageTitle = text_layer_create((GRect) { .origin = { 0, 72 }, .size = { bounds.size.w, 20 } });
+  splashMessage = text_layer_create((GRect) { .origin = { 0, 108 }, .size = { bounds.size.w, 40 } });
+  text_layer_set_text(splashImageTitle, "Not logged in");
+  text_layer_set_overflow_mode(splashMessage, GTextOverflowModeWordWrap);
+  text_layer_set_text(splashMessage, "Log in on Android and restart app");
   text_layer_set_text_alignment(splashImageTitle, GTextAlignmentCenter);
+  text_layer_set_text_alignment(splashMessage, GTextAlignmentCenter);
   layer_add_child(window_layer, text_layer_get_layer(splashImageTitle));
-  
+  layer_add_child(window_layer, text_layer_get_layer(splashMessage));
 }
 void splash_window_unload(Window *window){
   text_layer_destroy(splashImageTitle);
 }
+
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  //might not even use this
+}
+ 
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+}
+
 void splash_window_create(){
   splashWindow = window_create();
+  window_set_click_config_provider(splashWindow, click_config_provider);
   window_set_window_handlers(splashWindow, (WindowHandlers){
     .load = splash_window_load,
     .unload = splash_window_unload
