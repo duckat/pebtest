34530c27291a35f498af8b6a01e491897feb2543
duckat <anthonysunghoonkim@gmail.com>
2016-10-22 17:36:08 -0400

    window transition works

--- src/c/mainWindow.c
+++ src/c/mainWindow.c
@@ -2,9 +2,15 @@
 #include "mainWindow.h"
 
 Window *mainWindow;
+TextLayer *mainTitleText;
 
 void main_window_load(Window *window){
+  Layer *window_layer = window_get_root_layer(window);
   
+  mainTitleText = text_layer_create(GRect(0, 0, 144, 168));
+  text_layer_set_text(mainTitleText, "Main window!");
+  text_layer_set_text_alignment(mainTitleText, GTextAlignmentCenter);
+  layer_add_child(window_layer, text_layer_get_layer(mainTitleText));
 }
 void main_window_unload(Window *window){
 
