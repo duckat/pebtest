c76f3ef977cce903c16a8ef61bb231d9a858970a
dmill118 <dmiller182@gmail.com>
2016-10-22 23:43:22 -0400

    Added communication code for splash screen
    Not sure if this works yet, right now the app will hang at the start screen without any input from Android.

--- src/c/main.c
+++ src/c/main.c
@@ -8,20 +8,8 @@ int logged_in = true;
 // void launch_main_window(void *data){
 
 // }
-static void init(){
-  static PebbleAppHandlers s_handlers = {
-    .messaging_info = {
-      .buffer_sizes = {
-        .inbound = 64,
-        .outbound = 16
-      }
-      .default_callbacks.callbacks{
-        .out_sent = my_out_sent_handler,
-        
-      }
-    }
-  }
-}
+
+
 void launch_balance_window(void *data){
   balance_window_create();
   window_stack_push(balance_window_get_window(), true);
@@ -54,24 +42,36 @@ void click_config_provider(void *context)
 }
 
 
+void start_splash_window() {
+    splash_window_create();
+    window_stack_push(splash_window_get_window(), true);
+}
 
-void check_login(){
-  //query android for login status
-  if (logged_in) 
-  {
+void start_main_window() {
     main_window_create();
     window_set_click_config_provider(main_window_get_window(), click_config_provider);
     window_stack_push(main_window_get_window(), true);
-  } else {
-    splash_window_create();
-    window_stack_push(splash_window_get_window(), true);
-  }
+}
+
+void init(){
+  //Register AppMessage events
+  app_message_register_inbox_received(in_received_handler);           
+  app_message_open(512, 512);    //Large input and output buffer sizes
+}
+
+static void in_received_handler(DictionaryIterator *iter, void *context) 
+{
+    Tuple *t = dict_read_first(iter);
+    if(t)
+    {
+        start_main_window();
+    }
 }
 
 int main(){
   //app_timer_register(1500, launch_main_window, NULL);
   init();
-  check_login();
+  start_splash_window();
 
   app_event_loop();
   splash_window_destroy();
